3b2357242c6ad63d720413d293c4ee8337c9a570
RazvanAnisia <anisiarazvan@gmail.com>
2020-10-21 19:46:32 +0100

    added linting comments to ignore logs

--- src/middleware/requestLoggerMiddleware.ts
+++ src/middleware/requestLoggerMiddleware.ts
@@ -5,10 +5,12 @@ const requestLoggerMiddleware = (
   res: Response,
   next: NextFunction,
 ) => {
+  /* tslint:disable-next-line */
   console.info(req.method + req.originalUrl);
   const start = new Date().getTime();
   req.on('finish', () => {
     const elapsed = new Date().getTime() - start;
+    /* tslint:disable-next-line */
     console.info(
       `${req.method} + ${req.originalUrl} + ${res.statusCode} ${elapsed}ms`,
     );
